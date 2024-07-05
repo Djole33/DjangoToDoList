3d7f22d16b1320d03d359dbe0f28fd66ef9a7b2f
Djole33 <pandajedetrsku@gmail.com>
2024-07-05 18:03:32 +0200

    added form and database funcionality for adding tasks

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import *
+from .forms import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'main/home.html')
+    tasks = Tasks.objects.all()
+    form = TaskForm()
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+    return render(request, 'main/home.html', context={'tasks':tasks, 'form':form})
