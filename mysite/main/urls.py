60abaa86a8c7590e5de5cb03ce5769a6523ed582
Djole33 <pandajedetrsku@gmail.com>
2024-07-06 17:53:36 +0200

    added whole CRUD functionality

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
+    path('delete/<str:pk>/', views.deleteTask, name="delete"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 
 from .models import *
 from .forms import *
-
 # Create your views here.
 
 def home(request):
@@ -14,3 +13,20 @@ def home(request):
             form.save()
         return redirect("/")
     return render(request, 'main/home.html', context={'tasks':tasks, 'form':form})
+
+def updateTask(request, pk):
+    task = Tasks.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    return render(request, 'main/update_task.html', context={'form':form})
+
+def deleteTask(request, pk):
+    item = Tasks.objects.get(id=pk)
+    if request.method == "POST":
+        item.delete()
+        return redirect("/")
+    return render(request, 'main/delete.html', context={'item':item})
